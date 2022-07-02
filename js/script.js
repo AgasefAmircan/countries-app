394c1d2fe5c043730553d1242d9ece1c2a215bb4
agasefamircan <agasefemircanov@gmail.com>
2022-07-03 03:45:25 +0400

    Initial commmit

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function showCountry(data) {
   <div class="img"><img src=${data.flags.png} alt="" />
          </div>
           <div class="info">
-            <h4>${data.name.common}</h4>
+            <h4 class="searchName">${data.name.common}</h4>
             <p><b>Population:</b>${data.population}</p>
             <p class="regionName"><b>Region:</b> ${data.region}</p>
             <p><b>Capital:</b>${data.capital}</p>
@@ -37,7 +37,7 @@ function showCountry(data) {
 }
 
 
- //  Filter section open & close
+ //  Filter section  open & close
 
  const filter =document.querySelector('.filter');
  const dropDown=document.querySelector('.dropdown')
@@ -47,22 +47,35 @@ function showCountry(data) {
 
 
 // Filter section select country
-const regionName=document.querySelectorAll('.regionName');
+const regionName=document.getElementsByClassName('regionName');
 const region=document.querySelectorAll('.region')
 
+region.forEach(element =>{
+  element.addEventListener('click',()=>{
+    Array.from(regionName).forEach(elem =>{
+      if(elem.innerText.includes(element.innerText) || element.innerText=='All'){
+        elem.parentElement.parentElement.style.display="grid";
+      }
+      else{
+        elem.parentElement.parentElement.style.display="none";
+      }
+    })
+  })
+})
 
-// region.forEach(element=>{
-//   element.addEventListener("click",()=>{
-//    console.log(element);
-//    Array.from(regionName).forEach(elem =>{
-//     console.log(elem.insertAdjacentText);
-//     if(elem.insertAdjacentText.includes(element.insertAdjacentText) || element.insertAdjacentText=="All"){
-//       elem.parentElement.parentElement.parentElement.style.display="grid";
-//     }
-//     else{
-//       elem.parentElement.parentElement.style.display="none";
-//     }
-//    })
-//   })
-// })
 
+// Search sectiion
+const search=document.querySelectorAll('.search');
+const searchName=document.querySelectorAll('.searchName');
+
+search.addEventListener("input",()=>{
+  console.log(search.value.toLowerCase());
+  Array.from(searchName).forEach(elem =>{
+    if(elem.innerText.toLowerCase().includes(search.value.toLowerCase())){
+      elem.parentElement.parentElement.style.display="grid";
+    }
+    else{
+      elem.parentElement.parentElement.style.display="none";
+    }
+  });
+})
